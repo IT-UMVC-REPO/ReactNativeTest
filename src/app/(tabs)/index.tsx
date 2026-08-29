2999f3c872d4abeea2da986e33ea3c076c18e171
jmoran146006tc-svg <j.moran.146006.tc@umindanao.edu.ph>
2026-08-29 18:26:50 +0800

    feat: stuff and more stuff

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView)
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-blue-100">
+    <SafeAreaView className="flex-1 items-center justify-center bg-blue-100 p-5">
       <Text className="text-xl font-bold text-red-500">
         Welcome to Nativewind!
       </Text>
@@ -35,6 +38,6 @@ export default function App() {
       >
         Claude Pro Max Ultra
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
--- src/app/(tabs)/status.tsx
+++ src/app/(tabs)/status.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from 'react-native'
+import "@/global.css";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView)
 
 const status = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-blue-100 p-5">
       <Text>status</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
